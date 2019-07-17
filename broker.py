cfef63e9bbd64b2636a8e5b3ec057a7ad61269b3
tskluzac <skluzacek@uchicago.edu>
2019-07-16 23:40:46 -0500

    Working.

--- broker.py
+++ broker.py
@@ -2,12 +2,17 @@
 import zmq
 import pickle
 import time
+import random
+import logging
+from queue import Queue
 
 
 # TODO 1: Support having multiple container types connected simultaneously.
 # TODO 2: Spin up containers automatically using subprocess (like process worker pool).
 # TODO 3: Use actual containers with the workers (if container_reuse or container_single_use).
 # TODO 4: Track resources on the system (can then use this for scheduling purposes).
+
+# TODO: think of all workers as waiting for either WORK or KILL.
 
 # TYLER: Look in funcX worker for wrapping/unwrapping.
 class Worker(object):
@@ -22,11 +27,20 @@ class Worker(object):
         self.w_type = None
 
 
+class WorkerPool(object):
+    # k-v: work_type - task_q
+    worker_types = {}
+
+    # k-v: worker_id - socket_url
+    workers = {}
+
+    # k-v: worker_id - capacity (usually 0 or 1)
+    worker_capacities = {}
+
+
 class Broker(object):
 
     def __init__(self):
-        self.worker_types = {}  # k-v == worker_type + list of id's
-        self.workers = {}  # k-v == worker_id + resource reqs.
 
         self.context = zmq.Context()
         self.worker_socket = self.context.socket(zmq.ROUTER)
@@ -46,11 +60,20 @@ class Broker(object):
         print("Hey2")
         return "Pepperoni"
 
+    def create_new_workers(self, w_type):
+        port_range = range(50010, 54999)
+        new_port = random.choice(port_range)
+        print("CREATE")
+
+    def kill_workers(self):
+        print("KILL")
+
 
 broker = Broker()
 broker.worker_socket.bind("tcp://*:50001")
 broker.client_socket.bind("tcp://*:50002")
 
+worker_pool = WorkerPool()
 
 b_tasks = []
 results = []
@@ -79,12 +102,18 @@ while True:
     try:
         client_msg = broker.client_socket.recv_multipart(flags=zmq.NOBLOCK)
         print(client_msg)
-        task_id = pickle.loads(client_msg[1])
-        client_tasks = pickle.loads(client_msg[2])
-        client_command = pickle.loads(client_msg[3])
+        task_id = pickle.loads(client_msg[1])  # TODO: remove.
 
-        for task in client_tasks:
-            b_tasks.append((task_id, task))
+        b_tasks.append(client_msg)
+
+        #client_tasks = pickle.loads(client_msg[2])
+        #client_command = pickle.loads(client_msg[3])
+
+        # # TODO: Dont' worry about picking apart batches yet.
+        # for task in client_tasks:
+        #     b_tasks.append((task_id, task))
+
+            # {task_typeA: a_queue, task_typeB: b...}
 
     except zmq.ZMQError:
         print("No client messages")
@@ -93,25 +122,23 @@ while True:
     # If we have a message from worker, process it.
     if worker_msg is not None:
 
+        # TODO: Read the first-n bytes instead.
         task_id = pickle.loads(worker_msg[1])
         worker_result = pickle.loads(worker_msg[2])
         worker_command = pickle.loads(worker_msg[3])
 
         # On registration, create worker and add to worker dicts.
         if worker_command == "REGISTER":
-            broker.workers[worker_result["wid"]] = {
-                # "con_id": worker_result["con_id"],
-                # "gpu_avail": worker_result["gpu_avail"],
-                # "mem_avail": worker_result["mem_avail"],
-                # "data_obj": worker_result["data_obj"],
-                "last_result": "REGISTER",
-                "w_type": worker_result["w_type"]
-            }
 
-            if worker_result["w_type"] in broker.worker_types:
-                broker.worker_types["w_type"].append(worker_result["wid"])
-            else:
-                broker.worker_types["w_type"] = [worker_result["wid"]]
+            # Add worker from its new port.
+            worker_pool.workers[worker_result["wid"]] = "55555"  # TODO: Grab random port from spin-up.
+
+            # Set our initial capacity to 1, meaning we're ready for a task.
+            worker_pool.worker_capacities[worker_result["wid"]] = 1
+
+            # If we don't have a queue for this type of work yet, create one!
+            if worker_result["w_type"] not in worker_pool.worker_types:
+                worker_pool.worker_types["w_type"] = Queue()
 
             print("Successfully registered worker! ")
 
@@ -131,10 +158,12 @@ while True:
     if client_msg is not None:
         print("SENDING TASKS TO WORKER!")
         if len(b_tasks) > 0:
-            print(b_tasks)
             for task in b_tasks:
                 # Schema: worker_type, task_id, task_buffer (list).
-                broker.worker_socket.send_multipart([b"A", task[0], task[1].encode()])
+                # TODO: un-hardcode the 'A'
+                print(task)
+                task[0] = b"A"
+                broker.worker_socket.send_multipart(task)
                 print(task)
         else:
             print("NO TASKS")
